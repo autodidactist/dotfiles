b818c9bedcfb2b0fd567a29207f007d7e5b185fc
shahid <shahid.xyz@tuta.io>
2022-05-25 10:38:38 +0530

    Struggle always

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo xbps-install -Su dbus elogind vim neovim alacritty exa lf ranger stow sof-firmware pipewire alsa-utils alsa-plugins xtools mpv xwallpaper sxiv gcc tcc fasm ghc libXinerama-devel libXft-devel freetype-devel wget dmenu sxhkd tmux xcape zsh zathura pcmanfm lxappearance imwheel zenity xorg ttf-ubuntu-font-family xsel xclip slim pavucontrol unzip archiver par atool zathura-pdf-mupdf fd fzf dust ytop sd tealdeer tokei procs bat tree hyperfine gdb cgdb ffmpegthumbnailer xz gc-devel libedit libedit-devel ocaml  chromium python3-devel python3-pip tlp jq pamixer dunst Imagemagick convert newsboat xcb-util-wm pandoc poppler pcmanfm tumbler lsof ripgrep
